3cdd4bf56011f20eac32ae9c3938ffbed2a7c35b
Sitanshu Anmol <109926055+SitanshuA091@users.noreply.github.com>
2025-01-04 02:54:33 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,21 +1,27 @@
-import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Dropout, Embedding, LSTM, add
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-#Encoder Part of the Model
-inputs1 = Input(shape=(4096,), name="image")
-features1 = Dropout(0.4)(inputs1)
-features2 = Dense(256, activation='relu')(features1)
-#For Text Captions
-inputs2 = Input(shape=(Max_length,), name="text")
-sequences1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
-sequences2 = Dropout(0.4)(sequences1)
-sequences3 = LSTM(256)(sequences2)
+from keras.layers import Input, Dense, LSTM, Embedding, Dropout, add
+from keras.models import Model
 
-#Decoder Part of the Model
-decoder1 = add([features2, sequences3])
-decoder2 = Dense(256, activation='relu')(decoder1)
-outputs = Dense(vocab_size, activation='softmax')(decoder2)
+def Img_to_Cap_modelmodel(vocab_size, max_length):
+    """Define the image captioning model."""
+    # Feature extractor model input
+    inputs1 = Input(shape=(4096,))
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
 
-model = Model(inputs=[inputs1, inputs2], outputs=outputs)
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+    # Sequence model input
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256)(se2)
+
+    # Decoder model
+    decoder1 = add([fe2, se3])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+    # Merge the models
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    return model
+
+if __name__ == "__main__":
+    print("Model module loaded.")
